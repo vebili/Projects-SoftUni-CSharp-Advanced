280f386c4f062fb0b38d79f0225b2617432d3450
vebili <74007431+vebili@users.noreply.github.com>
2021-01-16 20:25:03 +0200

    Add project files.

---
+++ 01.CSharp-Advanced-Stacks-and-Queues/07.HotPatato/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.HotPatato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+            Queue<string> kids = new Queue<string>(input);
+            int counter = 0;
+            while (kids.Count > 1)
+            {
+                counter++;
+                string kid = kids.Dequeue();
+                if (counter == n)
+                {
+                    counter = 0;
+                    Console.WriteLine("Removed " + kid);
+                }
+                else
+                {
+                    kids.Enqueue(kid);
+                }
+            }
+            Console.WriteLine("Last is " + kids.Dequeue());
+        }
+    }
+}
