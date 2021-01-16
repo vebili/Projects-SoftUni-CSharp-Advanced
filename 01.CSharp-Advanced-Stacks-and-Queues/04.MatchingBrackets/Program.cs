32b0bfabda28f546fa86e09a804b1a9a6af0ac3f
vebili <74007431+vebili@users.noreply.github.com>
2021-01-16 19:38:37 +0200

    Add project files.

---
+++ 01.CSharp-Advanced-Stacks-and-Queues/04.MatchingBrackets/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<int> bracketPositions = new Stack<int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                int openingBracket = 0;
+                if (input[i] == '(')
+                {
+                    bracketPositions.Push(i);
+                }
+                else if (input[i] == ')')
+                {
+                    openingBracket = bracketPositions.Pop();
+                    Console.WriteLine(input.Substring(openingBracket, i - openingBracket + 1));
+                }
+            }
+        }
+    }
+}
