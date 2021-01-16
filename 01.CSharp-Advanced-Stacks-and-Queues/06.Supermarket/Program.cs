24e26dfafbef6cb56f50861780eecb8b5b7e765f
vebili <74007431+vebili@users.noreply.github.com>
2021-01-16 20:14:58 +0200

    Add project files.

---
+++ 01.CSharp-Advanced-Stacks-and-Queues/06.Supermarket/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Queue<string> name = new Queue<string>();
+            while (input != "End")
+            {
+                if (input == "Paid")
+                {
+                    foreach (var item in name)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    name.Clear();
+                }
+                else
+                {
+                    name.Enqueue(input);
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{name.Count} people remaining.");
+        }
+    }
+}
