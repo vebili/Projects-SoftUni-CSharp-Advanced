a502779071efca941e61105090dc10dd1ea7cafa
vebili <74007431+vebili@users.noreply.github.com>
2021-01-16 20:49:57 +0200

    Add project files.

--- 01.CSharp-Advanced-Stacks-and-Queues/08.TrafficJam/Program.cs
+++ 01.CSharp-Advanced-Stacks-and-Queues/08.TrafficJam/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _08.TrafficJam
 {
@@ -6,7 +7,31 @@ namespace _08.TrafficJam
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+            Queue<string> cars = new Queue<string>();
+            int numberOfCars = 0;
+            while (command != "end")
+            {
+                if (command == "green")
+                {
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (cars.Count > 0)
+                        {
+                            Console.WriteLine($"{cars.Dequeue()} passed!");
+                            numberOfCars++;
+                        }
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(command);
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"{numberOfCars} cars passed the crossroads.");
         }
     }
 }
