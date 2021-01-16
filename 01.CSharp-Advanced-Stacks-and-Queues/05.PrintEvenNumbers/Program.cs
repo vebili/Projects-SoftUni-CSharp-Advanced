38988fde700363cedcae609e5c9dcba04436ec99
vebili <74007431+vebili@users.noreply.github.com>
2021-01-16 19:53:09 +0200

    Add project files.

--- 01.CSharp-Advanced-Stacks-and-Queues/05.PrintEvenNumbers/Program.cs
+++ 01.CSharp-Advanced-Stacks-and-Queues/05.PrintEvenNumbers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _05.PrintEvenNumbers
 {
@@ -6,7 +8,21 @@ namespace _05.PrintEvenNumbers
     {
         static void Main(string[] args)
         {
-            
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> numbers = new Queue<int>(input);
+            List<int> evens = new List<int>();
+            while (numbers.Count > 0)
+            {
+                if (numbers.Peek() % 2 == 0)
+                {
+                    evens.Add(numbers.Dequeue());
+                }
+                else
+                {
+                    numbers.Dequeue();
+                }
+            }
+            Console.WriteLine(string.Join(", ", evens));
         }
     }
 }
