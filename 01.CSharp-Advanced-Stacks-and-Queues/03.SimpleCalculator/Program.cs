62314dc9d4ee7ddc00bac4144577d79aaad7c6d6
vebili <74007431+vebili@users.noreply.github.com>
2021-01-16 19:20:42 +0200

    Add project files.

---
+++ 01.CSharp-Advanced-Stacks-and-Queues/03.SimpleCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().Reverse().ToArray();
+            Stack<string> result = new Stack<string>(input);
+            while (result.Count > 1)
+            {
+                int calc = 0;
+                int first = int.Parse(result.Pop());
+                string operation = result.Pop();
+                int second = int.Parse(result.Pop());
+                if (operation == "+")
+                {
+                    calc = first + second;
+                }
+                else
+                {
+                    calc = first - second;
+                }
+                result.Push(calc.ToString());
+            }
+            Console.WriteLine(result.Pop().ToString());
+        }
+    }
+}
