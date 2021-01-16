491c5ad356c3564d60e5af318571f9bde9c770c0
vebili <74007431+vebili@users.noreply.github.com>
2021-01-16 19:58:18 +0200

    Add project files.

---
+++ 01.CSharp-Advanced-Stacks-and-Queues/05.PrintEvenNumbers1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.PrintEvenNumbers1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            Queue<int> evens = new Queue<int>(numbers.Where(x => x % 2 == 0));
+            Console.WriteLine(string.Join(", ", evens));
+        }
+    }
+}
