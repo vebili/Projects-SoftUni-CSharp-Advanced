178d3fe9c059ac655cf802a6304051ce5ade5bba
vebili <74007431+vebili@users.noreply.github.com>
2021-01-19 14:05:53 +0200

    Exercise

--- 01.CSharp-Advanced-Stacks-and-Queues-Exercises/09.SimpleTextEditor1/Program.cs
+++ 01.CSharp-Advanced-Stacks-and-Queues-Exercises/09.SimpleTextEditor1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace _09.SimpleTextEditor1
 {
@@ -6,7 +8,37 @@ namespace _09.SimpleTextEditor1
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+            Stack<string> text = new Stack<string>();
+            StringBuilder strBuild = new StringBuilder();
+            text.Push(strBuild.ToString());
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string command = input[0];
+                if (command == "1")
+                {
+                    strBuild.Append(input[1]);
+                    text.Push(strBuild.ToString());
+                }
+                else if (command == "2")
+                {
+                    int count = int.Parse(input[1]);
+                    strBuild.Remove(strBuild.Length - count, count);
+                    text.Push(strBuild.ToString());
+                }
+                else if (command == "3")
+                {
+                    int index = int.Parse(input[1]);
+                    Console.WriteLine(strBuild[index - 1]);
+                }
+                else
+                {
+                    text.Pop();
+                    strBuild = new StringBuilder();
+                    strBuild.Append(text.Peek());
+                }
+            }
         }
     }
 }
