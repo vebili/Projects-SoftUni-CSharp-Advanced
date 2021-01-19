24e378f109c8c840493c47173d9f3e29aae9402c
vebili <74007431+vebili@users.noreply.github.com>
2021-01-19 10:59:32 +0200

    Exercises

---
+++ 01.CSharp-Advanced-Stacks-and-Queues-Exercises/07.TruckTour/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberPetrolPumps = int.Parse(Console.ReadLine());
+            Queue<int[]> pumps = new Queue<int[]>();
+            for (int i = 0; i < numberPetrolPumps; i++)
+            {
+                int[] dataForPump = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                pumps.Enqueue(dataForPump);
+            }
+            int index = 0;
+
+            while (true)
+            {
+                int totalfuel = 0;
+                foreach (var pump in pumps)
+                {
+                    int fuel = pump[0];
+                    int distance = pump[1];
+
+                    totalfuel += fuel - distance;
+
+                    if (totalfuel < 0)
+                    {
+                        int[] currentPump = pumps.Dequeue();
+                        pumps.Enqueue(currentPump);
+                        index++;
+                        break;
+                    }
+                }
+                if (totalfuel >= 0)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(index);
+        }
+    }
+}
