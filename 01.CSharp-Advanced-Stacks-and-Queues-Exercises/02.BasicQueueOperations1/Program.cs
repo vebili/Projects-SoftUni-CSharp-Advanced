db4017c822fa21509b915d6180cdc3b81a391915
vebili <74007431+vebili@users.noreply.github.com>
2021-01-19 10:03:18 +0200

    Exercises

---
+++ 01.CSharp-Advanced-Stacks-and-Queues-Exercises/02.BasicQueueOperations1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.BasicQueueOperations1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nsx = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>(numbers);
+            int dequeue = nsx[1];
+            int x = nsx[2];
+            for (int i = 0; i < dequeue; i++)
+            {
+                queue.Dequeue();
+            }
+            if (queue.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else if (queue.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                Console.WriteLine(queue.Min());
+            }
+        }
+    }
+}
