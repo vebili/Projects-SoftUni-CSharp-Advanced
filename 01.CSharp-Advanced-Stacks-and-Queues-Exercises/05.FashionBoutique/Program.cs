933784e801e62f2e9be1096365c83a3cab1886d1
vebili <74007431+vebili@users.noreply.github.com>
2021-01-19 10:51:47 +0200

    Exercises

---
+++ 01.CSharp-Advanced-Stacks-and-Queues-Exercises/05.FashionBoutique/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> clothes = new Stack<int>(input);
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int usedRacks = 1;
+            int currentSum = 0;
+            if (clothes.Count == 0 || rackCapacity == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+            while (clothes.Count > 0)
+            {
+                int currentValue = clothes.Peek();
+
+                if (currentValue + currentSum == rackCapacity)
+                {
+                    clothes.Pop();
+                    usedRacks++;
+                    currentSum = 0;
+                    if (clothes.Count == 0)
+                    {
+                        usedRacks--;
+                    }
+                }
+                else if (currentValue + currentSum < rackCapacity)
+                {
+                    currentSum += clothes.Pop();
+
+                }
+                else
+                {
+                    usedRacks++;
+                    currentSum = clothes.Pop();
+                }
+            }
+            Console.WriteLine(usedRacks);
+        }
+    }
+}
