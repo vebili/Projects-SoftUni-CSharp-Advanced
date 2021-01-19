13ecfedbb5bd57a605b181f7d885750fd54fe521
vebili <74007431+vebili@users.noreply.github.com>
2021-01-19 13:49:33 +0200

    Exercises

---
+++ 01.CSharp-Advanced-Stacks-and-Queues-Exercises/08.BalancedParenthesis1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.BalancedParenthesis1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> bracketStack = new Stack<char>();
+            foreach (var symbol in input)
+            {
+                if (bracketStack.Any())
+                {
+                    char check = bracketStack.Peek();
+                    if (check == '{' && symbol == '}')
+                    {
+                        bracketStack.Pop();
+                        continue;
+                    }
+                    else if (check == '[' && symbol == ']')
+                    {
+                        bracketStack.Pop();
+                        continue;
+                    }
+                    else if (check == '(' && symbol == ')')
+                    {
+                        bracketStack.Pop();
+                        continue;
+                    }
+                }
+                bracketStack.Push(symbol);
+            }
+            Console.WriteLine(!bracketStack.Any() ? "YES" : "NO");
+        }
+    }
+}
