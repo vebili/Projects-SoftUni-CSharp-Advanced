d8ebe4a78c26a3a0bca02d5167040e5d3e4c776e
vebili <74007431+vebili@users.noreply.github.com>
2021-01-20 00:03:44 +0200

    Exercises

---
+++ 01.CSharp-Advanced-Stacks-and-Queues-Exercises/11.KeyRevolver/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11.KeyRevolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bulletPrice = int.Parse(Console.ReadLine());
+            int sizeGunBarrel = int.Parse(Console.ReadLine());
+            int[] bulletsInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> bullets = new Stack<int>(bulletsInput);
+            int[] locksInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> locks = new Queue<int>(locksInput);
+            int valueIntelligence = int.Parse(Console.ReadLine());
+            int counterBullets = 0;
+
+            while (locks.Count != 0 && bullets.Count != 0)
+            {
+                int currentBullet = bullets.Pop();
+                int currentLock = locks.Peek();
+                counterBullets++;
+
+                if (currentBullet <= currentLock)
+                {
+                    locks.Dequeue();
+                    Console.WriteLine("Bang!");
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+
+                if (bullets.Count == 0)
+                {
+                    break;
+                }
+
+                if (counterBullets == sizeGunBarrel)
+                {
+                    Console.WriteLine("Reloading!");
+                    counterBullets = 0;
+                }
+            }
+            if (locks.Count == 0)
+            {
+                int firedBullets = bulletsInput.Length - bullets.Count;
+                int moneyEarned = valueIntelligence - firedBullets * bulletPrice;
+                Console.WriteLine($"{bullets.Count} bullets left. Earned ${moneyEarned}");
+            }
+            else
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+            }
+        }
+    }
+}
