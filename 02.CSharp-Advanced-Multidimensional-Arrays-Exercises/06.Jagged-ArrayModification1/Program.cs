b12849fec1d304ec513bc90179902883a9252aa6
vebili <74007431+vebili@users.noreply.github.com>
2021-01-23 13:42:53 +0200

    Add project files.

--- 02.CSharp-Advanced-Multidimensional-Arrays-Exercises/06.Jagged-ArrayModification1/Program.cs
+++ 02.CSharp-Advanced-Multidimensional-Arrays-Exercises/06.Jagged-ArrayModification1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _06.Jagged_ArrayModification1
 {
@@ -6,7 +7,53 @@ namespace _06.Jagged_ArrayModification1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int rows = int.Parse(Console.ReadLine());
+            int[][] jagged = new int[rows][];
+            for (int row = 0; row < rows; row++)
+            {
+                int[] rowData = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < rowData.Length; col++)
+                {
+                    jagged[row] = rowData;
+                }
+            }
+            string command = Console.ReadLine();
+            while (command != "END")
+            {
+                string[] arguments = command.Split();
+                string action = arguments[0];
+                int row = int.Parse(arguments[1]);
+                int col = int.Parse(arguments[2]);
+                int value = int.Parse(arguments[3]);
+                if (row >= 0 && col >= 0 && row < rows && col < jagged[row].Length)
+                {
+                    switch (action)
+                    {
+                        case "Add":
+                            jagged[row][col] += value;
+                            break;
+                        case "Subtract":
+                            jagged[row][col] -= value;
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+                command = Console.ReadLine();
+            }
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < jagged[row].Length; col++)
+                {
+                    Console.Write(jagged[row][col] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
