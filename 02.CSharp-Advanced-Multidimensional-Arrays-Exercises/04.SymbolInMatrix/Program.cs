f75ea04a09609a01bbcb57433cbb34fefa318529
vebili <74007431+vebili@users.noreply.github.com>
2021-01-23 13:31:44 +0200

    Add project files.

--- 02.CSharp-Advanced-Multidimensional-Arrays-Exercises/04.SymbolInMatrix/Program.cs
+++ 02.CSharp-Advanced-Multidimensional-Arrays-Exercises/04.SymbolInMatrix/Program.cs
@@ -6,7 +6,46 @@ namespace _04.SymbolInMatrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int dimensions = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[dimensions, dimensions];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] elements = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = elements[col];
+                }
+            }
+            char toFind = Console.ReadLine().ToCharArray()[0];
+            int elementRow = -1;
+            int elementCol = -1;
+            bool isFound = false;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    char element = matrix[row, col];
+                    if (element == toFind)
+                    {
+                        elementRow = row;
+                        elementCol = col;
+                        isFound = true;
+                        break;
+                    }
+                    if (isFound)
+                    {
+                        break;
+                    }
+                }
+            }
+            if (isFound)
+            {
+                Console.WriteLine($"({elementRow}, {elementCol})");
+            }
+            else
+            {
+                Console.WriteLine($"{toFind} does not occur in the matrix ");
+            }
         }
     }
 }
---
+++ 02.CSharp-Advanced-Multidimensional-Arrays-Exercises/04.SymbolInMatrix1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _04.SymbolInMatrix1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+            for (int row = 0; row < n; row++)
+            {
+                string rowData = Console.ReadLine();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+            string symbol = Console.ReadLine();
+            bool isFound = false;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col].ToString() == symbol)
+                    {
+                        isFound = true;
+                        Console.WriteLine($"({row}, {col})");
+                        break;
+                    }
+                }
+                if (isFound)
+                {
+                    break;
+                }
+            }
+            if (!isFound)
+            {
+                Console.WriteLine($"{symbol} does not occur in the matrix ");
+            }
+        }
+    }
+}
