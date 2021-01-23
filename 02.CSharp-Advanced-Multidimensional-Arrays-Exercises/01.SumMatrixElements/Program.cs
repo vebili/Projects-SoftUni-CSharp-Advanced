8377071551029d379f5b616c90503e45f0d1f1d7
vebili <74007431+vebili@users.noreply.github.com>
2021-01-23 13:23:37 +0200

    Add project files.

--- 02.CSharp-Advanced-Multidimensional-Arrays-Exercises/01.SumMatrixElements/Program.cs
+++ 02.CSharp-Advanced-Multidimensional-Arrays-Exercises/01.SumMatrixElements/Program.cs
@@ -7,12 +7,18 @@ namespace _01.SumMatrixElements
     {
         static void Main(string[] args)
         {
-            int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int[] input = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
             int[,] matrix = new int[input[0], input[1]];
             int sum = 0;
             for (int row = 0; row < matrix.GetLength(0); row++)
             {
-                int[] colData = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                int[] colData = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
                 for (int col = 0; col < matrix.GetLength(1); col++)
                 {
                     matrix[row, col] = colData[col];
--- 02.CSharp-Advanced-Multidimensional-Arrays-Exercises/02.SumMatrixColumns1/Program.cs
+++ 02.CSharp-Advanced-Multidimensional-Arrays-Exercises/02.SumMatrixColumns1/Program.cs
@@ -1,29 +1,26 @@
 ﻿using System;
 using System.Linq;
 
-namespace _02.SumMatrixColumns
+namespace _02.SumMatrixColumns1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] dimensions = Console.ReadLine()
-                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+            int[] size = Console.ReadLine()
+                .Split(", ")
                 .Select(int.Parse)
                 .ToArray();
-            int rows = dimensions[0];
-            int cols = dimensions[1];
-            int[,] matrix = new int[rows, cols];
+            int[,] matrix = new int[size[0], size[1]];
             for (int row = 0; row < matrix.GetLength(0); row++)
             {
-                int[] elements = Console.ReadLine()
-                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                int[] colData = Console.ReadLine()
+                    .Split()
                     .Select(int.Parse)
                     .ToArray();
                 for (int col = 0; col < matrix.GetLength(1); col++)
                 {
-                    matrix[row, col] = elements[col];
-
+                    matrix[row, col] = colData[col];
                 }
             }
             for (int col = 0; col < matrix.GetLength(1); col++)
