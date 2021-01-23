a1988ef69a9a331988bac0ede8a1b162b641df0f
vebili <74007431+vebili@users.noreply.github.com>
2021-01-23 13:28:04 +0200

    Add project files.

--- 02.CSharp-Advanced-Multidimensional-Arrays-Exercises/03.PrimaryDiagonal1/Program.cs
+++ 02.CSharp-Advanced-Multidimensional-Arrays-Exercises/03.PrimaryDiagonal1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _03.PrimaryDiagonal1
 {
@@ -6,7 +7,29 @@ namespace _03.PrimaryDiagonal1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            int sum = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] rowData = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+            //diagonal calculation
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (row == col)
+                    {
+                        sum += matrix[row, col];
+                    }
+                }
+            }
+            Console.WriteLine(sum);
         }
     }
 }
