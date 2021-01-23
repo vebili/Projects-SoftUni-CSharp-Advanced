9506838e7664ca82fbb07b0f23b064b4c133659d
vebili <74007431+vebili@users.noreply.github.com>
2021-01-23 14:40:18 +0200

    Exercise project files.

--- 02.CSharp-Advanced-Multidimensional-Arrays-Exercises/07.KnightGame/Program.cs
+++ 02.CSharp-Advanced-Multidimensional-Arrays-Exercises/07.KnightGame/Program.cs
@@ -6,7 +6,230 @@ namespace _07.KnightGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int dimensions = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[dimensions, dimensions];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] elements = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = elements[col];
+                }
+            }
+            int removedKnights = 0;
+            while (true)
+            {
+                int maxConflicts = 0;
+                int maxConflictsRow = -1;
+                int maxConflictsCol = -1;
+                int countConflictKnights = 0;
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        int currentConflicts = 0;
+                        char currentCell = matrix[row, col];
+                        if (currentCell == 'K')
+                        {
+                            if (IsUpLeftTaken(row, col, matrix))
+                            {
+                                currentConflicts++;
+                            }
+                            if (IsUpRightTaken(row, col, matrix))
+                            {
+                                currentConflicts++;
+                            }
+                            if (IsLeftUpTaken(row, col, matrix))
+                            {
+                                currentConflicts++;
+                            }
+                            if (IsLeftDownTaken(row, col, matrix))
+                            {
+                                currentConflicts++;
+                            }
+                            if (IsDownLeftTaken(row, col, matrix))
+                            {
+                                currentConflicts++;
+                            }
+                            if (IsDownRightTaken(row, col, matrix))
+                            {
+                                currentConflicts++;
+                            }
+                            if (IsRightUpTaken(row, col, matrix))
+                            {
+                                currentConflicts++;
+                            }
+                            if (IsRightDownTaken(row, col, matrix))
+                            {
+                                currentConflicts++;
+                            }
+                            if (currentConflicts > maxConflicts)
+                            {
+                                maxConflicts = currentConflicts;
+                                maxConflictsRow = row;
+                                maxConflictsCol = col;
+                                countConflictKnights++;
+                            }
+                        }
+                    }
+                }
+                if (countConflictKnights == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    matrix[maxConflictsRow, maxConflictsCol] = '0';
+                    removedKnights++;
+                }
+            }
+            Console.WriteLine(removedKnights);
+        }
+        private static void Print(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+        private static bool IsRightDownTaken(int row, int col, char[,] matrix)
+        {
+            if (row + 1 > matrix.GetLength(0) - 1
+                || col + 2 > matrix.GetLength(1) - 1)
+            {
+                return false;
+            }
+
+            if (matrix[row + 1, col + 2] == '0')
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private static bool IsRightUpTaken(int row, int col, char[,] matrix)
+        {
+            if (row - 1 < 0
+                || col + 2 > matrix.GetLength(1) - 1)
+            {
+                return false;
+            }
+
+            if (matrix[row - 1, col + 2] == '0')
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private static bool IsDownRightTaken(int row, int col, char[,] matrix)
+        {
+            if (row + 2 > matrix.GetLength(0) - 1
+                || col + 1 > matrix.GetLength(1) - 1)
+            {
+                return false;
+            }
+
+            if (matrix[row + 2, col + 1] == '0')
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private static bool IsDownLeftTaken(int row, int col, char[,] matrix)
+        {
+            if (row + 2 > matrix.GetLength(0) - 1
+                || col - 1 < 0)
+            {
+                return false;
+            }
+
+            if (matrix[row + 2, col - 1] == '0')
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private static bool IsLeftDownTaken(int row, int col, char[,] matrix)
+        {
+            if (row + 1 > matrix.GetLength(0) - 1
+                || col - 2 < 0)
+            {
+                return false;
+            }
+
+            if (matrix[row + 1, col - 2] == '0')
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private static bool IsLeftUpTaken(int row, int col, char[,] matrix)
+        {
+            if (row - 1 < 0
+                || col - 2 < 0)
+            {
+                return false;
+            }
+
+            if (matrix[row - 1, col - 2] == '0')
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private static bool IsUpRightTaken(int row, int col, char[,] matrix)
+        {
+            if (row - 2 < 0
+                || col + 1 > matrix.GetLength(1) - 1)
+            {
+                return false;
+            }
+
+            if (matrix[row - 2, col + 1] == '0')
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private static bool IsUpLeftTaken(int row, int col, char[,] matrix)
+        {
+            if (row - 2 < 0
+                || col - 1 < 0)
+            {
+                return false;
+            }
+            if (matrix[row - 2, col - 1] == '0')
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
--- 02.CSharp-Advanced-Multidimensional-Arrays-Exercises/07.KnightGame1/Program.cs
+++ 02.CSharp-Advanced-Multidimensional-Arrays-Exercises/07.KnightGame1/Program.cs
@@ -6,7 +6,97 @@ namespace _07.KnightGame1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            char[,] board = new char[n, n];
+            for (int row = 0; row < n; row++)
+            {
+                char[] rowData = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < n; col++)
+                {
+                    board[row, col] = rowData[col];
+                }
+            }
+            int removedCount = 0;
+            while (true)
+            {
+                int maxAttackedKnightsCount = 0;
+                int knightRow = -1;
+                int knightCol = -1;
+                for (int row = 0; row < n; row++)
+                {
+                    for (int col = 0; col < n; col++)
+                    {
+                        char symbol = board[row, col];
+                        if (symbol != 'K')
+                        {
+                            continue;
+                        }
+                        int count = GetCountOfAttackedKnights(board, row, col);
+                        if (count > maxAttackedKnightsCount)
+                        {
+                            maxAttackedKnightsCount = count;
+                            knightRow = row;
+                            knightCol = col;
+                        }
+                    }
+                }
+                if (maxAttackedKnightsCount == 0)
+                {
+                    break;
+                }
+                board[knightRow, knightCol] = '0';
+                removedCount++;
+            }
+            Console.WriteLine(removedCount);
+        }
+        private static int GetCountOfAttackedKnights(char[,] board, int row, int col)
+        {
+            int count = 0;
+            if (ContainsKnight(board, row - 2, col - 1))
+            {
+                count++;
+            }
+            if (ContainsKnight(board, row - 2, col + 1))
+            {
+                count++;
+            }
+            if (ContainsKnight(board, row - 1, col - 2))
+            {
+                count++;
+            }
+            if (ContainsKnight(board, row - 1, col + 2))
+            {
+                count++;
+            }
+            if (ContainsKnight(board, row + 1, col - 2))
+            {
+                count++;
+            }
+            if (ContainsKnight(board, row + 1, col + 2))
+            {
+                count++;
+            }
+            if (ContainsKnight(board, row + 2, col - 1))
+            {
+                count++;
+            }
+            if (ContainsKnight(board, row + 2, col + 1))
+            {
+                count++;
+            }
+            return count;
+        }
+        private static bool ContainsKnight(char[,] board, int row, int col)
+        {
+            if (!isValidCell(row, col, board.GetLength(0)))
+            {
+                return false;
+            }
+            return board[row, col] == 'K';
+        }
+        private static bool isValidCell(int row, int col, int lenght)
+        {
+            return row >= 0 && row < lenght && col >= 0 && col < lenght;
         }
     }
 }
