c167a1aed6f8dbbb90b72fbee1abf4432567ffcc
vebili <74007431+vebili@users.noreply.github.com>
2021-01-23 12:44:06 +0200

    Addproject files.

---
+++ 02.CSharp-Advanced-Multidimensional-Arrays-Exercises/03.PrimaryDiagonal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
