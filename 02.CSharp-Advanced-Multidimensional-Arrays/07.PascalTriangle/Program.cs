af3eca1ab602fe1c09434645068ee87d8220c285
vebili <74007431+vebili@users.noreply.github.com>
2021-01-23 13:53:20 +0200

    Lab

--- 02.CSharp-Advanced-Multidimensional-Arrays/07.PascalTriangle/Program.cs
+++ 02.CSharp-Advanced-Multidimensional-Arrays/07.PascalTriangle/Program.cs
@@ -6,7 +6,25 @@ namespace _07.PascalTriangle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberRows = int.Parse(Console.ReadLine());
+            long[][] triangle = new long[numberRows][];
+            for (int row = 0; row < numberRows; row++)
+            {
+                triangle[row] = new long[row + 1];
+            }
+            triangle[0][0] = 1;
+            for (int row = 0; row < numberRows - 1; row++)
+            {
+                for (int col = 0; col <= row; col++)
+                {
+                    triangle[row + 1][col] += triangle[row][col];
+                    triangle[row + 1][col + 1] += triangle[row][col];
+                }
+            }
+            for (int row = 0; row < numberRows; row++)
+            {
+                Console.WriteLine(string.Join(" ", triangle[row]));
+            }
         }
     }
 }
