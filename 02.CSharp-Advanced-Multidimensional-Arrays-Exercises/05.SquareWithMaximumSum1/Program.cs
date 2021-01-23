70bdeab290c536728e50189c93d13e1c5021c1fe
vebili <74007431+vebili@users.noreply.github.com>
2021-01-23 13:33:59 +0200

    Add project files.

---
+++ 02.CSharp-Advanced-Multidimensional-Arrays-Exercises/05.SquareWithMaximumSum1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.SquareWithMaximumSum1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+            int[,] matrix = new int[size[0], size[1]];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] rowData = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+            int maxRow = 0;
+            int maxCol = 0;
+            int maxSum = int.MinValue;
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    int sum = matrix[row, col] + matrix[row + 1, col] + matrix[row, col + 1] + matrix[row + 1, col + 1];
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        maxRow = row;
+                        maxCol = col;
+                    }
+                }
+            }
+            for (int row = maxRow; row < maxRow + 2; row++)
+            {
+                for (int col = maxCol; col < maxCol + 2; col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(maxSum);
+        }
+    }
+}
