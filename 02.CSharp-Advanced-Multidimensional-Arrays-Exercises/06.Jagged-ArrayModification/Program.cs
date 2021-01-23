22635a3f17ca283a79fcf56b335eb5f4f993c15b
vebili <74007431+vebili@users.noreply.github.com>
2021-01-23 12:46:04 +0200

    Add project files.

---
+++ 02.CSharp-Advanced-Multidimensional-Arrays-Exercises/06.Jagged-ArrayModification/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06.Jagged_ArrayModification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
