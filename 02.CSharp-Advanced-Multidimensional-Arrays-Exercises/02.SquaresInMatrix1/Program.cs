b991d56a2b62af59be1cadaabaccc52efd79aa35
vebili <74007431+vebili@users.noreply.github.com>
2021-01-23 14:55:41 +0200

    Exercise project files.

--- 02.CSharp-Advanced-Multidimensional-Arrays-Exercises/02.SquaresInMatrix1/Program.cs
+++ 02.CSharp-Advanced-Multidimensional-Arrays-Exercises/02.SquaresInMatrix1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _02.SquaresInMatrix1
 {
@@ -6,7 +7,31 @@ namespace _02.SquaresInMatrix1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] size = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            string[,] matrix = new string[size[0], size[1]];
+            for (int row = 0; row < size[0]; row++)
+            {
+                string[] rowValues = Console.ReadLine().Split();
+                for (int col = 0; col < size[1]; col++)
+                {
+                    matrix[row, col] = rowValues[col];
+                }
+            }
+            int count = 0;
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    if ((matrix[row, col] == matrix[row, col + 1]) && (matrix[row + 1, col] == matrix[row + 1, col + 1]) && (matrix[row, col] == matrix[row + 1, col + 1]))
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
         }
     }
 }
