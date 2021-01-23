92d2ca94f343d1188fb98e0dc19b39eb13441fe8
vebili <74007431+vebili@users.noreply.github.com>
2021-01-23 13:51:37 +0200

    Lab

---
+++ 02.CSharp-Advanced-Multidimensional-Arrays/07.PascalTriangle1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _07.PascalTriangle1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            long[][] PTriangle = new long[rows][];
+            int initValue = 1;
+            for (long row = 0; row < rows; row++)
+            {
+                PTriangle[row] = new long[initValue];
+                long[] currentRow = PTriangle[row];
+                currentRow[0] = 1;
+                currentRow[currentRow.Length - 1] = 1;
+                initValue++;
+                if (currentRow.Length > 2)
+                {
+                    for (int i = 1; i < currentRow.Length - 1; i++)
+                    {
+                        long[] previousRow = PTriangle[row - 1];
+                        long previousRowSum = previousRow[i] + previousRow[i - 1];
+                        currentRow[i] = previousRowSum;
+                    }
+                }
+            }
+            foreach (var item in PTriangle)
+            {
+                Console.WriteLine(string.Join(" ", item));
+            }
+        }
+    }
+}
