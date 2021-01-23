91f6d82f9575b7c3df66877c32e8bab6c833605e
vebili <74007431+vebili@users.noreply.github.com>
2021-01-23 14:51:47 +0200

    Exercise project files.

--- 02.CSharp-Advanced-Multidimensional-Arrays-Exercises/01.DiagonalDifference/Program.cs
+++ 02.CSharp-Advanced-Multidimensional-Arrays-Exercises/01.DiagonalDifference/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _01.DiagonalDifference
 {
@@ -6,7 +7,34 @@ namespace _01.DiagonalDifference
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int rows = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[rows, rows];
+            for (int row = 0; row < rows; row++)
+            {
+                int[] currentRow = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < rows; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+            int sum1 = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                int currCol = row;
+                sum1 += matrix[row, currCol];
+            }
+            int sum2 = 0;
+            int col2 = rows - 1;
+            for (int row = 0; row < rows; row++)
+            {
+                sum2 += matrix[row, col2];
+                col2--;
+            }
+            int diff = Math.Abs(sum1 - sum2);
+            Console.WriteLine(diff);
         }
     }
 }
--- 02.CSharp-Advanced-Multidimensional-Arrays-Exercises/01.DiagonalDifference1/Program.cs
+++ 02.CSharp-Advanced-Multidimensional-Arrays-Exercises/01.DiagonalDifference1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _01.DiagonalDifference1
 {
@@ -6,7 +7,33 @@ namespace _01.DiagonalDifference1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            for (int row = 0; row < n; row++)
+            {
+                int[] rowData = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+            int sumLeftDiagonal = 0;
+            int sumRightDiagonal = 0;
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (row == col)
+                    {
+                        sumLeftDiagonal += matrix[row, col];
+                    }
+                    if ((row + col) == n - 1)
+                    {
+                        sumRightDiagonal += matrix[row, col];
+                    }
+                }
+            }
+            Console.WriteLine(Math.Abs(sumLeftDiagonal - sumRightDiagonal)); ;
         }
     }
 }
