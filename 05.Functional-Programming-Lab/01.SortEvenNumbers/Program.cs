c08b3d9927c7019f3f59fd7fa85c25146da4f2ca
vebili <74007431+vebili@users.noreply.github.com>
2021-01-29 21:29:06 +0200

    Add project files.

---
+++ 05.Functional-Programming-Lab/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.SortEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                .OrderBy(x => x)
+                .ToList();
+
+            Console.WriteLine(string.Join(", ", nums));
+        }
+    }
+}
