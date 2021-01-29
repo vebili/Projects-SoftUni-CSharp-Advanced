ce13d2c2dc68459b4b4e6da4591e7f5487ad8784
vebili <74007431+vebili@users.noreply.github.com>
2021-01-29 22:29:41 +0200

    Add project files.

--- 05.Functional-Programming-Lab/05.FilterByAge1/Program.cs
+++ 05.Functional-Programming-Lab/05.FilterByAge1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _05.FilterByAge1
 {
@@ -6,7 +7,70 @@ namespace _05.FilterByAge1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var people = GetPeople();
+            var oldOrYoung = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+            var format = Console.ReadLine();
+            Func<int, bool> tester = CreateTester(oldOrYoung, age);
+            Action<KeyValuePair<string, int>> printer = CreatePrinter(format);
+            InvokePrinter(people, tester, printer);
+        }
+        private static void InvokePrinter(Dictionary<string, int> people, Func<int, bool> tester, Action<KeyValuePair<string, int>> printer)
+        {
+            foreach (var person in people)
+            {
+                if (tester(person.Value))
+                {
+                    printer(person);
+                }
+            }
+        }
+        private static Action<KeyValuePair<string, int>> CreatePrinter(string format)
+        {
+            //return format switch
+            //{
+            //    "name age" => kvp => Console.WriteLine($"{kvp.Key} - {kvp.Value}"),
+            //    "name" => kvp => Console.WriteLine($"{kvp.Key}"),
+            //    "age" => kvp => Console.WriteLine($"{kvp.Value}"),
+            //    _ => null,
+            //};
+            switch (format)
+            {
+                case "name age":
+                    return kvp => Console.WriteLine($"{kvp.Key} - {kvp.Value}");
+                case "name":
+                    return kvp => Console.WriteLine($"{kvp.Key}");
+                case "age":
+                    return kvp => Console.WriteLine($"{kvp.Value}");
+                default:
+                    return null;
+            }
+        }
+        private static Func<int, bool> CreateTester(string oldOrYoung, int age)
+        {
+            switch (oldOrYoung)
+            {
+                case "older":
+                    return n => n >= age;
+                case "younger":
+                    return n => n < age;
+                default:
+                    return null;
+            }
+        }
+        private static Dictionary<string, int> GetPeople()
+        {
+            var numberOfLines = int.Parse(Console.ReadLine());
+            var people = new Dictionary<string, int>(numberOfLines);
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                var input = Console.ReadLine()
+                    .Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                people[input[0]] = int.Parse(input[1]);
+            }
+            return people;
         }
     }
 }
