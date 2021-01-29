85a136287b0ac831d85924188e023b994117c9fe
vebili <74007431+vebili@users.noreply.github.com>
2021-01-29 21:57:14 +0200

    Add project files.

---
+++ 05.Functional-Programming-Lab/02.SumNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> myFunc = int.Parse;
+            var numbers = Console.ReadLine()
+                .Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(myFunc)
+                .ToList();
+
+            Console.WriteLine(numbers.Count);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
