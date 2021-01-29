4faa24ea8b00abecd30313e4cffd5d862546714c
vebili <74007431+vebili@users.noreply.github.com>
2021-01-29 22:00:09 +0200

    Add project files.

---
+++ 05.Functional-Programming-Lab/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> myFunc = word => char.IsUpper(word[0]);
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(myFunc)
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
