085906dae6c0d459a7d655dca97dbdcc8d36671e
vebili <74007431+vebili@users.noreply.github.com>
2021-01-29 22:02:21 +0200

    Add project files.

---
+++ 05.Functional-Programming-Lab/04.AddVAT/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prices = Console.ReadLine()
+                 .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(double.Parse)
+                 .Select(p => p * 1.2)
+                 .Select(p => new string($"{p:f2}"))
+                 .ToList();
+            var stringResults = prices.Select(x => new string($"{x:f2}")).ToArray();
+            var result = string.Join(Environment.NewLine, prices);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 05.Functional-Programming-Lab/05.FilterByAge/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.FilterByAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var people = new List<Person>();
+            for (int i = 0; i < n; i++)
+            {
+                var personArgs = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                var person = new Person
+                {
+                    Name = personArgs[0],
+                    Age = int.Parse(personArgs[1])
+                };
+                people.Add(person);
+            }
+            var filter = Console.ReadLine();
+            var ages = int.Parse(Console.ReadLine());
+            Func<Person, bool> myFilter = filter switch
+            {
+                "older" => p => p.Age >= ages,
+                "younger" => p => p.Age < ages,
+                _ => p => true
+            };
+            var outputFormat = Console.ReadLine();
+            Func<Person, string> outputFunc = outputFormat switch
+            {
+                "name age" => p => $"{p.Name} - {p.Age}",
+                "name" => p => p.Name,
+                "age" => p => p.Age.ToString(),
+                _ => null
+            };
+            var sortingFormat = Console.ReadLine();
+            var sortFunc = sortingFormat switch
+            {
+                "name" => people.OrderBy(p => p.Name),
+                "age" => people.OrderBy(p => p.Age),
+                _ => people.OrderBy(p => p)
+            };
+
+
+            people
+                .Where(myFilter)
+                .Select(outputFunc)
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
