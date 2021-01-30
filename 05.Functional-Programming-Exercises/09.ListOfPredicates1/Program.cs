1bb0d5a74f720163aca6513ee7f6f398dc9932c5
vebili <74007431+vebili@users.noreply.github.com>
2021-01-30 22:18:16 +0200

    Add project files.

---
+++ 05.Functional-Programming-Exercises/09.ListOfPredicates1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09.ListOfPredicates1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
