6b4ff2bf5640e92495fc9c0cd0b5a0d3a52ef850
vebili <74007431+vebili@users.noreply.github.com>
2021-01-30 22:02:29 +0200

    Add project files.

--- 05.Functional-Programming-Exercises/02.KnightsofHonor/Program.cs
+++ 05.Functional-Programming-Exercises/02.KnightsofHonor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _02.KnightsofHonor
 {
@@ -6,7 +7,11 @@ namespace _02.KnightsofHonor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Action<string> Print = msg => Console.WriteLine($"Sir {msg}");
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .ForEach(Print);
         }
     }
 }
