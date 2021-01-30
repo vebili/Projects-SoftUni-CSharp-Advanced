efec5eab480c350c2a9ce5c398e8531f12d72f00
vebili <74007431+vebili@users.noreply.github.com>
2021-01-30 22:16:49 +0200

    Add project files.

--- 05.Functional-Programming-Exercises/04.FindEvensorOdds/Program.cs
+++ 05.Functional-Programming-Exercises/04.FindEvensorOdds/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _04.FindEvensorOdds
 {
@@ -6,7 +8,25 @@ namespace _04.FindEvensorOdds
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] borders = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int lowerBorder = Math.Min(borders[0], borders[1]);
+            int upperBorder = Math.Max(borders[0], borders[1]);
+            string filter = Console.ReadLine();
+            Predicate<int> predicate = n => filter == "odd" ?
+                n % 2 != 0 :
+                n % 2 == 0;
+            List<int> result = new List<int>();
+            for (int i = lowerBorder; i <= upperBorder; i++)
+            {
+                if (predicate(i))
+                {
+                    result.Add(i);
+                }
+            }
+            Console.WriteLine(string.Join(" ", result));
         }
     }
 }
