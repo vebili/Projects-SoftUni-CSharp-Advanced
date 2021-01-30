725848932ab095e79fb0fb44a84ed0ecfa395000
vebili <74007431+vebili@users.noreply.github.com>
2021-01-30 22:10:41 +0200

    Add project files.

---
+++ 05.Functional-Programming-Exercises/06.ReverseAndExclude1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06.ReverseAndExclude1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
