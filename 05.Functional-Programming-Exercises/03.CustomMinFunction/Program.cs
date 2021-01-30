674aa47d7d734a0dfc7950a144166cc9e2806a38
vebili <74007431+vebili@users.noreply.github.com>
2021-01-30 21:59:56 +0200

    Add project files.

---
+++ 05.Functional-Programming-Exercises/03.CustomMinFunction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
