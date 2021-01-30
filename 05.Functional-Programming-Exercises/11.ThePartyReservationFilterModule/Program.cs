d40ece131b172ad62fb9a4e8d1598d7b89b8cd04
vebili <74007431+vebili@users.noreply.github.com>
2021-01-30 22:22:01 +0200

    Add project files.

---
+++ 05.Functional-Programming-Exercises/11.ThePartyReservationFilterModule/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _11.ThePartyReservationFilterModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 05.Functional-Programming-Exercises/11.ThePartyReservationFilterModule1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _11.ThePartyReservationFilterModule1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
