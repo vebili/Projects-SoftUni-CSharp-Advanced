01503e50ba05761196a96f92d579ae1a4a60d3d0
vebili <74007431+vebili@users.noreply.github.com>
2021-01-31 18:36:39 +0200

    Add project files.

--- 05.Functional-Programming-Exercises/12.TriFunction/Program.cs
+++ 05.Functional-Programming-Exercises/12.TriFunction/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _12.TriFunction
 {
@@ -6,7 +7,15 @@ namespace _12.TriFunction
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int targetASCII = int.Parse(Console.ReadLine());
+            string[] inputNames = Console.ReadLine().Split();
+            Action<string> Print = name => Console.WriteLine(name);
+            Func<string, int, bool> IsSumEqual =
+                        (name, ascii) => name.Sum(c => c) >= ascii;
+            Func<string[], Func<string, int, bool>, string> FindFirstName =
+                        (names, IsSumEqualASCII) => names.FirstOrDefault(n => IsSumEqual(n, targetASCII));
+            string result = FindFirstName(inputNames, IsSumEqual);
+            Print(result);
         }
     }
 }
