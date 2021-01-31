707c39ccafea41ed36c6dc17e3da3008c6bc391d
vebili <74007431+vebili@users.noreply.github.com>
2021-01-31 18:20:48 +0200

    Add project files.

--- 05.Functional-Programming-Exercises/05.AppliedArithmetics1/Program.cs
+++ 05.Functional-Programming-Exercises/05.AppliedArithmetics1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _05.AppliedArithmetics1
 {
@@ -6,7 +8,34 @@ namespace _05.AppliedArithmetics1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            string command = Console.ReadLine();
+            while (command != "end")
+            {
+                switch (command)
+                {
+                    case "add":
+                        Func<int, int> addFunc = x => x + 1;
+                        input = Calculator(input, addFunc);
+                        break;
+                    case "multiply":
+                        Func<int, int> multiFunc = x => x * 2;
+                        input = Calculator(input, multiFunc);
+                        break;
+                    case "subtract":
+                        Func<int, int> subtFunc = x => x - 1;
+                        input = Calculator(input, subtFunc);
+                        break;
+                    case "print":
+                        Console.WriteLine(string.Join(" ", input));
+                        break;
+                }
+                command = Console.ReadLine();
+            }
         }
+        static List<int> Calculator(List<int> list, Func<int, int> passedFunc) => list.Select(passedFunc).ToList();
     }
 }
