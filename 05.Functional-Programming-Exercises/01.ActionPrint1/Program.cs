3ed00d9a2295688aa775f861278d04fee81c7c04
vebili <74007431+vebili@users.noreply.github.com>
2021-01-31 17:19:56 +0200

    Add project files.

--- 05.Functional-Programming-Exercises/01.ActionPrint1/Program.cs
+++ 05.Functional-Programming-Exercises/01.ActionPrint1/Program.cs
@@ -6,7 +6,16 @@ namespace _01.ActionPrint1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] strings = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Action<string> myPrint = PrintStrings;
+            for (int i = 0; i < strings.Length; i++)
+            {
+                myPrint(strings[i]);
+            }
+        }
+        private static void PrintStrings(string str)
+        {
+            Console.WriteLine(str);
         }
     }
 }
