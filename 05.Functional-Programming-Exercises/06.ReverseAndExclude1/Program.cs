ba0706e623105da25dca021991474399d78a9305
vebili <74007431+vebili@users.noreply.github.com>
2021-01-31 18:22:17 +0200

    Add project files.

--- 05.Functional-Programming-Exercises/06.ReverseAndExclude1/Program.cs
+++ 05.Functional-Programming-Exercises/06.ReverseAndExclude1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _06.ReverseAndExclude1
 {
@@ -6,7 +8,14 @@ namespace _06.ReverseAndExclude1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Reverse()
+                .ToList();
+            int n = int.Parse(Console.ReadLine());
+            numbers = numbers.Where(x => x % n != 0).ToList();
+            Console.WriteLine(string.Join(" ", numbers));
         }
     }
 }
