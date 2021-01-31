0278dbf57b7bb44f454d94cb04b5b2665a20bd48
vebili <74007431+vebili@users.noreply.github.com>
2021-01-31 18:26:47 +0200

    Add project files.

--- 05.Functional-Programming-Exercises/09.ListOfPredicates1/Program.cs
+++ 05.Functional-Programming-Exercises/09.ListOfPredicates1/Program.cs
@@ -6,7 +6,28 @@ namespace _09.ListOfPredicates1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int[] dividers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Func<int[], int, bool> tester = DivisionChecker;
+            for (int i = 1; i <= n; i++)
+            {
+                if (tester(dividers, i) == true)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+        private static bool DivisionChecker(int[] nums, int num)
+        {
+            bool output = true;
+            foreach (var item in nums)
+            {
+                if (num % item != 0)
+                {
+                    output = false;
+                }
+            }
+            return output;
         }
     }
 }
