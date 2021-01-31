87194871cb50b42553ad8b1f2d502ff037d12090
vebili <74007431+vebili@users.noreply.github.com>
2021-01-31 18:51:07 +0200

    Add project files.

--- 05.Functional-Programming-Exercises/10.PredicateParty!/Program.cs
+++ 05.Functional-Programming-Exercises/10.PredicateParty!/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _10.PredicateParty
 {
@@ -6,7 +8,61 @@ namespace _10.PredicateParty
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> guests = Console.ReadLine()
+                .Split()
+                .ToList();
+            Action<List<string>> Print = g => Console.WriteLine(string.Join(", ", g) + " are going to the party!");
+            Predicate<string> predicate;
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split();
+                if (input[0] == "Party!")
+                {
+                    break;
+                }
+                string command = input[0];
+                string predicateInput = input[1];
+                string value = input[2];
+                predicate = GetPredicate(predicateInput, value);
+                if (command == "Remove")
+                {
+                    guests.RemoveAll(predicate);
+                }
+                else
+                {
+                    List<string> filteredGuests = guests.FindAll(predicate);
+
+                    foreach (var guest in filteredGuests)
+                    {
+                        int index = guests.IndexOf(guest);
+                        guests.Insert(index + 1, guest);
+                    }
+                }
+            }
+            if (guests.Count > 0)
+            {
+                Print(guests);
+            }
+            else
+            {
+                Console.WriteLine("Nobody is going to the party!");
+            }
+        }
+        private static Predicate<string> GetPredicate(string predicateInput, string value)
+        {
+            if (predicateInput == "StartsWith")
+            {
+                return p => p.StartsWith(value);
+            }
+            else if (predicateInput == "EndsWith")
+            {
+                return p => p.EndsWith(value);
+            }
+            else if (predicateInput == "Length")
+            {
+                return p => p.Length == int.Parse(value);
+            }
+            return null;
         }
     }
 }
--- 05.Functional-Programming-Exercises/11.ThePartyReservationFilterModule1/Program.cs
+++ 05.Functional-Programming-Exercises/11.ThePartyReservationFilterModule1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _11.ThePartyReservationFilterModule1
 {
@@ -6,7 +8,68 @@ namespace _11.ThePartyReservationFilterModule1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var reservations = Console.ReadLine()
+                .Split()
+                .ToList();
+            List<string> filters = new List<string>();
+            string input;
+
+            while ((input = Console.ReadLine()) != "Print")
+            {
+                var cmdArgs = input.Split(";");
+                string command = cmdArgs[0];
+                string filter = cmdArgs[1];
+                string param = cmdArgs[2];
+                //filters= GetFilters(filters, command, filter, param);
+                switch (command)
+                {
+                    case "Add filter":
+                        filters.Add(filter + " " + param);
+
+                        break;
+                    case "Remove filter":
+                        if (filters.Contains(filter + " " + param))
+                            filters.Remove(filter + " " + param);
+                        break;
+                }
+            }
+            foreach (var item in filters)
+            {
+                string[] filterArgs = item.Split();
+                string filter = string.Empty;
+                string param = string.Empty;
+                if (filterArgs.Length == 3)
+                {
+                    filter = filterArgs[0] + " " + filterArgs[1];
+                    param = filterArgs[2];
+                }
+                else if (filterArgs.Length < 3)
+                {
+                    filter = filterArgs[0];
+                    param = filterArgs[1];
+                }
+                Predicate<string> predicate = GetPredicate(filter, param);
+                reservations.RemoveAll(predicate);
+            }
+            if (reservations.Any())
+                Console.WriteLine(string.Join(" ", reservations));
+        }
+        static Predicate<string> GetPredicate(string filter, string param)
+        {
+
+            Predicate<string> predicate = filter switch
+            {
+                "Starts with" => predicate = new Predicate<string>(name =>
+                  name.StartsWith(param)),
+                "Ends with" => predicate = new Predicate<string>(name =>
+                 name.EndsWith(param)),
+                "Length" => predicate = new Predicate<string>(name =>
+                 name.Length == int.Parse(param)),
+                "Contains" => predicate = new Predicate<string>(name =>
+                  name.Contains(param)),
+                _ => null
+            };
+            return predicate;
         }
     }
 }
