f4fea8efca7d115698d691cca923a11b1ac830b1
vebili <74007431+vebili@users.noreply.github.com>
2021-01-31 18:25:31 +0200

    Add project files.

--- 05.Functional-Programming-Exercises/08.CustomComparator1/Program.cs
+++ 05.Functional-Programming-Exercises/08.CustomComparator1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _08.CustomComparator1
 {
@@ -6,7 +7,23 @@ namespace _08.CustomComparator1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] nums = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Array.Sort(nums, Compare);
+            Console.WriteLine(string.Join(" ", nums));
+        }        static int Compare(int a, int b)
+        {
+            if (a % 2 != 0 && b % 2 == 0)
+            {
+                return 1;
+            }
+            else if (a % 2 == 0 && b % 2 != 0)
+            {
+                return -1;
+            }
+            return a.CompareTo(b);
         }
     }
 }
