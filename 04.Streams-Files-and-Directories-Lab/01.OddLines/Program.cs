2b51abb5086dff270f7ea0d7a396453b101757b6
vebili <74007431+vebili@users.noreply.github.com>
2021-02-01 15:00:31 +0200

    Add project files.

---
+++ 04.Streams-Files-and-Directories-Lab/01.OddLines/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace _01.OddLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path1 = Path.Combine("data", "Input.txt");
+            var path2 = Path.Combine("data", "Output.txt");
+            var reader = new StreamReader(path1);
+            using (reader)
+            {
+                int counter = 0;
+                string line = reader.ReadLine();
+                using (var writer = new StreamWriter(path2))
+                {
+                    while (line != null)
+                    {
+                        if (counter % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 04.Streams-Files-and-Directories-Lab/01.OddLines1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace _01.OddLines1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.Combine("data", "Input.txt");
+            string destination = Path.Combine("data", "Output.txt");
+
+            using (FileStream file = new FileStream(path, FileMode.Open))
+            {
+                using (TextReader reader = new StreamReader(file))
+                {
+                    using (FileStream fileWriteStream = new FileStream(destination, FileMode.OpenOrCreate))
+                    {
+                        using (TextWriter writer = new StreamWriter(fileWriteStream))
+                        {
+                            string line = reader.ReadLine();
+                            int counter = 0;
+                            while (line != null)
+                            {
+                                if (counter % 2 != 0)
+                                {
+                                    writer.WriteLine(line);
+                                }
+                                counter++;
+                                line = reader.ReadLine();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
