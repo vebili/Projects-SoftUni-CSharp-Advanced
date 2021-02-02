091bdb8894e952315b1cad695f0f0103b52bd2eb
vebili <74007431+vebili@users.noreply.github.com>
2021-02-02 15:55:40 +0200

    Add project files.

---
+++ 06.Defining-Classes-Lab/05.SpecialCars1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _05.SpecialCars1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
