0bf35a1d96dc69901a532e7796694e2c0f33f8cb
vebili <74007431+vebili@users.noreply.github.com>
2021-02-12 18:28:22 +0200

    Add project files.

---
+++ 04.Streams-Files-and-Directories-Lab/03.WordCount1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.WordCount1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
