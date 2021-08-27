7c78f8cbbc525c02bc9fccfd3f23e8b715b3dafe
vebili <74007431+vebili@users.noreply.github.com>
2021-08-27 15:25:06 +0300

    Add

---
+++ 11.Algorithms Introduction/06.QuickSort/Quicksort.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Quicksort
+
+{
+    public class Quick
+    {
+        public static void Sort<T>(T[] a)
+            where T : IComparable<T>
+        {
+
+            Sort<T>(a, 0, a.Length - 1);
+        }
+
+        private static void Sort<T>(T[] a, int lo, int hi)
+            where T : IComparable<T>
+        {
+            int i = lo;
+            int j = hi;
+            T pivot = a[(lo + hi) / 2];
+
+            while (i <= j)
+            {
+                while (a[i].CompareTo(pivot) < 0) i++;
+
+                while (a[j].CompareTo(pivot) > 0) j--;
+
+                if (i <= j)
+                {
+                    T tmp = a[i];
+                    a[i] = a[j];
+                    a[j] = tmp;
+                    i++;
+                    j--;
+                }
+            }
+
+            if (lo < j) Sort(a, lo, j);
+
+            if (i < hi) Sort(a, i, hi);
+        }
+
+        class Quicksort
+        {
+            static void Main(string[] args)
+            {
+                int[] array = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                Quick.Sort(array);
+
+                Console.WriteLine(string.Join(" ", array));
+            }
+        }
+    }
+}
---
+++ 11.Algorithms Introduction/07.BinarySearchIterative/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace BinarySearchIterative
+{
+    class Program
+    {
+       static int BinarySearch(int[] arr, int key, int start, int end)
+        {
+            while (end>=start)
+            {
+                int mid = (start + end) / 2;
+                if(arr[mid]<key)
+                {
+                    start = mid + 1;
+                }
+                else if(arr[mid]>key)
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+
+            int[] collection = Console.ReadLine()
+             .Split()
+             .Select(int.Parse)
+             .ToArray();
+            Array.Sort(collection);
+            int start = 0;
+            int end = collection.Length-1;
+            int key = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{BinarySearch(collection,key, start, end)}");
+
+        }
+    }
+}
