82c2c340d658a6f3b53157907cb276ba5d1600e4
vebili <74007431+vebili@users.noreply.github.com>
2021-08-28 19:32:26 +0300

    Add

---
+++ 09.Iterators and Comparators Exercise/01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> items;
+        private int index;
+
+        public ListyIterator(List<T> items)
+        {
+            this.items = items;
+            this.index = 0;
+        }
+
+        public bool Move()
+        {
+            if (this.HasNext())
+            {
+                this.index++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            if (index < this.items.Count - 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        public void Print()
+        {
+            if (this.items.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.WriteLine(this.items[this.index]);
+        }
+        
+    }
+}
---
+++ 09.Iterators and Comparators Exercise/01.ListyIterator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.ListyIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<string> listyIterator = null;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "END")
+                {
+                    break;
+                }
+
+                if (input.Contains("Create"))
+                {
+                    List<string> items = input
+                                            .Split()
+                                            .Skip(1)
+                                            .ToList();
+                    listyIterator = new ListyIterator<string>(items);
+                }
+                else if (input == "Print")
+                {
+                    try
+                    {
+                        listyIterator.Print();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else if (input == "HasNext")
+                {
+                    Console.WriteLine(listyIterator.HasNext());
+                    
+                }
+                else if (input == "Move")
+                {
+                    Console.WriteLine(listyIterator.Move());
+                }
+
+            }
+        }
+    }
+}
