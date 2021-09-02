ee4b9b7912733740545adbefe30a2431753cc246
vebili <74007431+vebili@users.noreply.github.com>
2021-09-02 09:32:53 +0300

    Add

---
+++ 06.Defining-Classes-Exercises/09. Pokemon Trainer/StartUp.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokemonTrainer
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Trainer> trainers = new Dictionary<string, Trainer>();
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                if (input[0] == "Tournament")
+                {
+                    break;
+                }
+
+                string trainerName = input[0];
+                string pokemonName = input[1];
+                string pokemonElement = input[2];
+                int pokemonHealth = int.Parse(input[3]);
+
+                Pokemon currentPokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+                Trainer currenTrainer = new Trainer(trainerName);
+
+                if (!trainers.ContainsKey(trainerName))
+                {
+                    trainers[trainerName] = currenTrainer;
+                }
+                trainers[trainerName].Pokemons.Add(currentPokemon);
+            }
+
+            while (true)
+            {
+                string element = Console.ReadLine();
+                if (element == "End")
+                {
+                    break;
+                }
+
+                foreach (var (name, trainer) in trainers)
+                {
+                    bool havePokemon = false;
+                    foreach (var pokemon in trainer.Pokemons)
+                    {
+                        if (pokemon.Element == element)
+                        {
+                            trainer.Badges += 1;
+                            havePokemon = true;
+                            break;
+                        }
+                    }
+
+                    if (havePokemon == false)
+                    {
+                        for (int i = 0; i < trainer.Pokemons.Count; i++)
+                        {
+                            if (trainer.Pokemons[i].Health > 10)
+                            {
+                                trainer.Pokemons[i].Health -= 10;
+                            }
+                            else
+                            {
+                                trainer.Pokemons.Remove(trainer.Pokemons[i]);
+                                i--;
+                            }
+                        }
+                    }
+                }
+            }
+
+            foreach (var (name, trainer) in trainers
+                                            .OrderByDescending(t => t.Value.Badges))
+            {
+                Console.WriteLine($"{trainer.Name} {trainer.Badges} {trainer.Pokemons.Count}");
+            }
+
+        }
+    }
+}
---
+++ 06.Defining-Classes-Exercises/09. Pokemon Trainer/Trainer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace PokemonTrainer
+{
+    public class Trainer
+    {
+        public Trainer(string trainerName)
+        {
+            Name = trainerName;
+            Badges = 0;
+            Pokemons = new List<Pokemon>();
+        }
+        public string Name { get; set; }
+        public int Badges { get; set; }
+        public List<Pokemon> Pokemons { get; set; }
+    }
+}
