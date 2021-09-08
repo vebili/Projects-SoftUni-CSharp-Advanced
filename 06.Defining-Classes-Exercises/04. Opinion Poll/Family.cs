5ba3e2a61b944b4fbf3a09969b24499f626f7fa1
vebili <74007431+vebili@users.noreply.github.com>
2021-09-08 11:55:05 +0300

    Add

---
+++ 06.Defining-Classes-Exercises/04. Opinion Poll/Family.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpinionPoll
+{
+    public class Family
+    {
+        private List<Person> family;
+
+        public Family()
+        {
+            this.family = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            this.family.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return family.OrderByDescending(p => p.Age).FirstOrDefault();
+        }
+
+        public List<Person> GetPollMembers()
+        {
+            List<Person> pollMembers = this.family.Where(f => f.Age > 30).OrderBy(f => f.Name).ToList();
+
+            return pollMembers;
+        }
+    }
+}
---
+++ 06.Defining-Classes-Exercises/04. Opinion Poll/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OpinionPoll
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int familyMembers = int.Parse(Console.ReadLine());
+            Family family = new Family();
+
+            for (int i = 0; i < familyMembers; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                Person currentMember = new Person(name, age);
+
+                family.AddMember(currentMember);
+            }
+
+            var newFamily = family.GetPollMembers();
+
+            foreach (var person in newFamily)
+            {
+                Console.WriteLine(person.ToString());
+            }
+           
+        }
+    }
+}
