69dad515f6f8930ee287bf65393961936b969250
vebili <74007431+vebili@users.noreply.github.com>
2022-01-30 09:01:55 +0200

    Add

---
+++ 04.Streams-Files-and-Directories-Exercise/02.LineNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"text.txt";
+            string outputPath = @"output.txt";
+
+            string[] lines = File.ReadAllLines(filePath);
+
+            int count = 1;
+
+            foreach (var line in lines)
+            {
+                int lettersCount = line.Count(char.IsLetter);
+                int puncsCount = line.Count(char.IsPunctuation);
+
+                File.AppendAllText(outputPath, $"Line {count}: {line} ({lettersCount})({puncsCount}){Environment.NewLine}");
+                count++;
+            }
+        }
+    }
+}
