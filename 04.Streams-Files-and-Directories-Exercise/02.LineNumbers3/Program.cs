df1ecf045bb7b96081b615d5c5f66d2511588a61
vebili <74007431+vebili@users.noreply.github.com>
2022-01-31 08:39:49 +0200

    Add

---
+++ 04.Streams-Files-and-Directories-Exercise/02.LineNumbers3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace _02.LineNumbers3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var reader = new StreamReader("text.txt");
+            using StreamWriter writer = new StreamWriter("output.txt", true);//it stays if false don't saves the changes just run it at the moment
+
+            int count = 1;
+
+            while (true)
+            {
+                var line = reader.ReadLine();
+                if (line == null)
+                {
+                    break;
+                }
+
+                string patternLetters = @"[A-Za-z]";
+                MatchCollection letters = Regex.Matches(line, patternLetters);
+                string patternPunctuation = @"[\.\,\!\?\-\']";
+                MatchCollection punctuations = Regex.Matches(line, patternPunctuation);
+                var convertedLine = $"Line{count}:{line} ({letters.Count})({punctuations.Count})";
+
+                writer.WriteLine(convertedLine);
+                count++;
+            }
+        }
+    }
+}
