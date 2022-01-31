b03615a18440584e4ead3d8bf8b07ee3ffff11cd
vebili <74007431+vebili@users.noreply.github.com>
2022-01-31 08:34:47 +0200

    Add

---
+++ 04.Streams-Files-and-Directories-Exercise/02.LineNumbers2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace _02.LineNumbers2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.Combine("inputText.txt");
+            string dest = Path.Combine("outputText.txt");
+            var lines = File.ReadAllLines(path);
+            int counter = 1;
+            string[] output = new string[lines.Length];
+            foreach (var line in lines)
+            {
+                output[counter - 1] = $"{counter}. {line}";
+                counter++;
+            }
+
+            File.WriteAllLines(dest, output);
+        }
+    }
+}
