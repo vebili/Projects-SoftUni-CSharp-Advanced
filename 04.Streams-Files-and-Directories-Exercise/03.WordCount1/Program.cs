730bc9277ffe452880cb0f2b9f2aa7ede1798b19
vebili <74007431+vebili@users.noreply.github.com>
2022-02-01 12:19:19 +0200

    Add

---
+++ 04.Streams-Files-and-Directories-Exercise/03.WordCount1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _03.WordCount1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> wordsCount = new Dictionary<string, int>();
+
+            string fileWords = @"words.txt";
+            string fileText = @"text.txt";
+            string fileActual = @"actualResult.txt";
+            string fileExpected = @"expectedResult.txt";
+
+            string text = File.ReadAllText(fileText);
+            string[] words = File.ReadAllLines(fileWords);
+
+            foreach (var word in words)
+            {
+                if (!wordsCount.ContainsKey(word))
+                {
+                    wordsCount[word] = 0;
+                }
+            }
+
+            string pattern = @"[A-Za-z]+";
+            Regex rg = new Regex(pattern);
+
+            foreach (Match match in rg.Matches(text.ToLower()))
+            {
+                if (wordsCount.ContainsKey(match.Value))
+                {
+                    wordsCount[match.Value]++;
+                }
+            }
+
+            foreach (var (key, value) in wordsCount)
+            {
+                File.AppendAllText(fileActual, $"{key} - {value}{Environment.NewLine}");
+            }
+
+            foreach (var (key, value) in wordsCount.OrderByDescending(x => x.Value))
+            {
+                File.AppendAllText(fileExpected, $"{key} - {value}{Environment.NewLine}");
+            }
+
+        }
+    }
+}
