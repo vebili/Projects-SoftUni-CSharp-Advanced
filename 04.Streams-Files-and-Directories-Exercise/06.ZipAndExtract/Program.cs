c0ea07850ce2eb3496b0c7a3bfc643d8d15b9b77
vebili <74007431+vebili@users.noreply.github.com>
2022-02-02 15:47:17 +0200

    Add

---
+++ 04.Streams-Files-and-Directories-Exercise/06.ZipAndExtract/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace _06.ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @".\copyMe.png";
+            string zipPath = @"D:\result.zip";
+            string pathExtractedFile = @"D:\";
+            ZipFile.CreateFromDirectory(filePath, zipPath);
+            ZipFile.ExtractToDirectory(zipPath, pathExtractedFile);
+        }
+    }
+}
---
+++ 04.Streams-Files-and-Directories-Exercise/06.ZipAndExtract2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace _06.ZipAndExtract2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "copyMe.png";
+            string pathDesktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string outputFileNAme = @"myZip.zip";
+            string outputPath = Path.Combine(pathDesktop, outputFileNAme);
+
+            using (ZipArchive archive = ZipFile.Open(outputPath, ZipArchiveMode.Create))
+            {
+                archive.CreateEntryFromFile(filePath, Path.GetFileName(filePath));
+            }
+        }
+    }
+}
