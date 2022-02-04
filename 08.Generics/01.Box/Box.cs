68732df6187c54607d766bc216894038f7511360
vebili <74007431+vebili@users.noreply.github.com>
2022-02-04 14:11:26 +0200

    Add

---
+++ 08.Generics/01.Box/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace _01.Box
+{
+    public class Box<T>
+    {
+        private List<T> boxItems;
+
+        public Box()
+        {
+            this.boxItems = new List<T>();
+        }
+
+        public int Count => this.boxItems.Count;
+
+        public void Add(T element)
+        {
+            boxItems.Add(element);
+        }
+
+        public T Remove()
+        {
+            T lastElement = boxItems.Last();
+            boxItems.RemoveAt(Count - 1);
+
+            return lastElement;
+
+        }
+    }
+}
---
+++ 08.Generics/01.Box/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.Box
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
