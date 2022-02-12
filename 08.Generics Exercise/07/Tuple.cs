39285c633b82be14b6fa6663d992fba500f753cf
vebili <74007431+vebili@users.noreply.github.com>
2022-02-12 10:20:58 +0200

    Add

---
+++ 08.Generics Exercise/07/Tuple.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _07.Tuple
+{
+    public class Tuple<T, V>
+    {
+        private T item1;
+        private V item2;
+
+        public Tuple(T item1, V item2)
+        {
+            this.item1 = item1;
+            this.item2 = item2;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{this.item1} -> {this.item2}");
+        }
+    }
+}
